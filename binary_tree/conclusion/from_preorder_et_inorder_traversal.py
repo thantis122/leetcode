c144ba685879a2a6ee39ce029ec578d63eefa9b8
LIU Bin <liubin122@hotmail.com>
2018-03-12 17:28:51 +0800

    update

--- binary_tree/conclusion/from_preorder_et_inorder_traversal.py
+++ binary_tree/conclusion/from_preorder_et_inorder_traversal.py
@@ -13,7 +13,7 @@ class TreeNode:
 
 
 class Solution:
-    def buildTree(self, preorder, inorder):
+    def buildTree(self, inorder, preorder):
         '''
 
         :type preorder: List[int]
@@ -27,3 +27,39 @@ class Solution:
             rroot.right = self.buildTree(inorder[rootPre + 1:], preorder)
             rroot.left = self.buildTree(inorder[:rootPre], preorder)
             return rroot
+
+
+
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        self.preorder = preorder
+        self.inorder = inorder
+        return self._buildTree(0, len(preorder), 0, len(inorder))
+
+    def _buildTree(self, pre_start, pre_end, in_start, in_end):
+        if pre_start >= pre_end:
+            return None
+        root = TreeNode(self.preorder[pre_start])
+        offset = self.inorder[in_start:in_end + 1].index(root.val)
+        root.left = self._buildTree(pre_start + 1, pre_start + offset + 1, in_start, in_start + offset)
+        root.right = self._buildTree(pre_start + offset + 1, pre_end, in_start + offset + 1, in_end)
+        return root
+
+
+if __name__ == "__main__":
+    None
